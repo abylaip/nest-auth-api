a6176ce454f784f7ad04a88c72e128c0f16a0b2f
Abylay Aiyp <ablyaip@gmail.com>
2022-03-20 00:12:20 +0600

    fix: gitignore

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,11 @@ import { RolesModule } from "src/roles/roles.module";
 import { UsersController } from "./users.controller";
 import { User } from "./users.model";
 import { UsersService } from "./users.service";
+import { UserRoles } from "src/roles/user-roles.model";
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Role]), RolesModule],
+  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
 })
 export class UsersModule {}
