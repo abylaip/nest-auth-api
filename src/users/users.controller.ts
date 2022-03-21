a6115ec6938ce73b329be40c9a5607c1c74d7d27
Abylay Aiyp <ablyaip@gmail.com>
2022-03-21 22:10:14 +0600

    feat: add pipes and validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+} from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 import { User } from "./users.model";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { Roles } from "src/auth/roles-auth.decorator";
 import { RolesGuard } from "src/auth/roles.guard";
 import { AddRoleDto } from "./dto/add-role.dto";
 import { BanUserDto } from "./dto/ban-user.dto";
+import { ValidationPipe } from "src/pipes/validation.pipe";
 
 @ApiTags("Пользователи")
 @Controller("users")
@@ -16,6 +23,7 @@ export class UsersController {
 
   @ApiOperation({ summary: "Создание пользователя" })
   @ApiResponse({ status: 200, type: User })
+  @UsePipes(ValidationPipe)
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
@@ -34,6 +42,7 @@ export class UsersController {
   @ApiResponse({ status: 200 })
   @Roles("ADMIN")
   @UseGuards(RolesGuard)
+  @UsePipes(ValidationPipe)
   @Post("/role")
   addRole(@Body() dto: AddRoleDto) {
     return this.usersService.addRole(dto);
