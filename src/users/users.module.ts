3979bab7b15801573cdb087cd0c6bedcd4c6c2c9
Abylay Aiyp <ablyaip@gmail.com>
2022-03-21 19:44:52 +0600

    feat: add guards for endpoints

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { Role } from "src/roles/roles.model";
 import { RolesModule } from "src/roles/roles.module";
@@ -6,11 +6,16 @@ import { UsersController } from "./users.controller";
 import { User } from "./users.model";
 import { UsersService } from "./users.service";
 import { UserRoles } from "src/roles/user-roles.model";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
+  imports: [
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
